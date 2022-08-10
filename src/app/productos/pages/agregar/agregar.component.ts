7ad5938cbb440f08a03d9e95190880f55c153e39
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-08-09 21:42:07 -0500

    Formulario reactivo tradicional

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html'
 })
-export class AgregarComponent implements OnInit {
+export class AgregarComponent {
 
-  constructor() { }
+  form: FormGroup = this.fb.group({
+    nombre: ['', Validators.required ]
+  });
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) { }
+
+  tieneError(campo: string): boolean {
+    return this.form.get(campo)?.invalid || false;
   }
-
 }
--- src/app/productos/productos.module.ts
+++ src/app/productos/productos.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { ProductosRoutingModule } from './productos-routing.module';
@@ -11,7 +12,8 @@ import { ProductosRoutingModule } from './productos-routing.module';
   ],
   imports: [
     CommonModule,
-    ProductosRoutingModule
+    ProductosRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductosModule { }
